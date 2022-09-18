4ec238d4b2eac984a1daf8b6d0bbc18fe0fb4f7d
Chimezie Ugonna <ugiezie@gmail.com>
2022-09-18 17:55:06 +0100

    minor fixes.

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -12,7 +12,7 @@ class LoginController extends Controller
     public function create(Request $request)
     {
         if (User::find($request->request->get("user_id"))) {
-            Login::updateOrCreate(["user_id" => $request->request->get("user_id"), "access_type" => $request->request->get("access_type"), "device_token" => $request->request->get("device_token")], $request->all());
+            Login::updateOrCreate(["user_id" => $request->request->get("user_id"), "access_type" => $request->request->get("access_type"), "device_os" => $request->request->get("device_os"), "device_token" => $request->request->get("device_token")], $request->all());
             $auth = new Authentication();
             return response()->json([
                 "status" => true,
@@ -63,7 +63,7 @@ class LoginController extends Controller
 
     public function delete(Request $request)
     {
-        Login::where("user_id", $request->request->get("user_id"))->where("access_type", request()->header("access_type"))->where("device_os", request()->header("device_os", ""))->where("device_token", request()->header("device_token", ""))->delete();
+        Login::where("user_id", $request->request->get("user_id"))->where("access_type", $request->header("access-type"))->where("device_os", $request->header("device-os", ""))->where("device_token", $request->header("device-token", ""))->delete();
         return response()->json([
             "status" => true,
             "message" => "User logged out successfully."
