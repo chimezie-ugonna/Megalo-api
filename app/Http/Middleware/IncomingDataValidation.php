5cab4e42c2a29cafea25f8211f2a0dfbb0b031cb
Chimezie Ugonna <ugiezie@gmail.com>
2022-09-19 08:03:44 +0100

    minor fixes.

--- app/Http/Middleware/IncomingDataValidation.php
+++ app/Http/Middleware/IncomingDataValidation.php
@@ -78,17 +78,75 @@ class IncomingDataValidation
                 $request->validate([
                     "property_id" => ["bail", "required", "filled", "not_in:null"]
                 ]);
-                if (sizeof($request->all()) == 1) {
+                if (sizeof($request->all()) <= 1) {
                     return response()->json([
                         "status" => false,
                         "message" => "There is nothing to update."
                     ], 400)->throwResponse();
-                } else if (sizeof($request->all()) > 1) {
-                    if (!$request->request->has("address") && !$request->request->has("value") && !$request->request->has("pictures") && !$request->request->has("available_shares") && !$request->request->has("size") && !$request->request->has("dividend")) {
-                        return response()->json([
-                            "status" => false,
-                            "message" => "You provided an invalid key."
-                        ], 400)->throwResponse();
+                } else if (!$request->request->has("address") && !$request->request->has("value_usd") && !$request->request->has("image_strings") && !$request->request->has("available_shares") && !$request->request->has("size_sf") && !$request->request->has("dividend_ps_usd")) {
+                    return response()->json([
+                        "status" => false,
+                        "message" => "You provided an invalid key."
+                    ], 400)->throwResponse();
+                } else if (!$request->filled("address") && !$request->filled("value_usd") && !$request->filled("image_strings") && !$request->filled("available_shares") && !$request->filled("size_sf") && !$request->filled("dividend_ps_usd")) {
+                    return response()->json([
+                        "status" => false,
+                        "message" => "There is no data to update."
+                    ], 400)->throwResponse();
+                } else {
+                    if ($request->request->has("address")) {
+                        if ($request->filled("address")) {
+                            $request->validate([
+                                "address" => ["bail", "not_in:null"]
+                            ]);
+                        } else {
+                            $request->request->remove("address");
+                        }
+                    }
+                    if ($request->request->has("value_usd")) {
+                        if ($request->filled("value_usd")) {
+                            $request->validate([
+                                "value_usd" => ["bail", "numeric", "not_in:null"]
+                            ]);
+                        } else {
+                            $request->request->remove("value_usd");
+                        }
+                    }
+                    if ($request->request->has("image_strings")) {
+                        if ($request->filled("image_strings")) {
+                            $request->validate([
+                                "image_strings" => ["bail", "not_in:null"]
+                            ]);
+                        } else {
+                            $request->request->remove("image_strings");
+                        }
+                    }
+                    if ($request->request->has("available_shares")) {
+                        if ($request->filled("available_shares")) {
+                            $request->validate([
+                                "available_shares" => ["bail", "numeric", "not_in:null"]
+                            ]);
+                        } else {
+                            $request->request->remove("available_shares");
+                        }
+                    }
+                    if ($request->request->has("size_sf")) {
+                        if ($request->filled("size_sf")) {
+                            $request->validate([
+                                "size_sf" => ["bail", "numeric", "not_in:null"]
+                            ]);
+                        } else {
+                            $request->request->remove("size_sf");
+                        }
+                    }
+                    if ($request->request->has("dividend_ps_usd")) {
+                        if ($request->filled("dividend_ps_usd")) {
+                            $request->validate([
+                                "dividend_ps_usd" => ["bail", "numeric", "not_in:null"]
+                            ]);
+                        } else {
+                            $request->request->remove("dividend_ps_usd");
+                        }
                     }
                 }
             } else if ($request->path() == "api/v1/user/update") {
@@ -107,26 +165,61 @@ class IncomingDataValidation
                         "status" => false,
                         "message" => "There is no data to update."
                     ], 400)->throwResponse();
-                } else if ($request->request->has("full_name") && $request->filled("full_name")) {
-                    $full_name_split = explode(" ", $request->request->get("full_name"), 2);
-                    $first_name = $full_name_split[0];
-                    $last_name = "";
-                    if (count($full_name_split) > 1) {
-                        $last_name = $full_name_split[1];
+                } else {
+                    if ($request->request->has("phone_number")) {
+                        if ($request->filled("phone_number")) {
+                            $request->validate([
+                                "phone_number" => ["bail", "not_in:null"]
+                            ]);
+                        } else {
+                            $request->request->remove("phone_number");
+                        }
                     }
-                    $request->request->add([
-                        "first_name" => $first_name,
-                        "last_name" => $last_name
-                    ]);
-                    $request->request->remove("full_name");
-                } else if ($request->request->has("dob") && $request->filled("dob")) {
-                    $request->validate([
-                        "dob" => ["bail", "date_format:d/m/Y", "not_in:null"]
-                    ]);
-                } else if ($request->request->has("type") && $request->filled("type")) {
-                    $request->validate([
-                        "type" => ["bail", "in:user,admin", "not_in:null"]
-                    ]);
+                    if ($request->request->has("full_name")) {
+                        if ($request->filled("full_name")) {
+                            $request->validate([
+                                "full_name" => ["bail", "not_in:null"]
+                            ]);
+                            $full_name_split = explode(" ", $request->request->get("full_name"), 2);
+                            $first_name = $full_name_split[0];
+                            $last_name = "";
+                            if (count($full_name_split) > 1) {
+                                $last_name = $full_name_split[1];
+                            }
+                            $request->request->add([
+                                "first_name" => $first_name,
+                                "last_name" => $last_name
+                            ]);
+                        }
+                        $request->request->remove("full_name");
+                    }
+                    if ($request->request->has("dob")) {
+                        if ($request->filled("dob")) {
+                            $request->validate([
+                                "dob" => ["bail", "date_format:d/m/Y", "not_in:null"]
+                            ]);
+                        } else {
+                            $request->request->remove("dob");
+                        }
+                    }
+                    if ($request->request->has("email")) {
+                        if ($request->filled("email")) {
+                            $request->validate([
+                                "email" => ["bail", "email", "not_in:null"]
+                            ]);
+                        } else {
+                            $request->request->remove("email");
+                        }
+                    }
+                    if ($request->request->has("type")) {
+                        if ($request->filled("type")) {
+                            $request->validate([
+                                "type" => ["bail", "in:user,admin", "not_in:null"]
+                            ]);
+                        } else {
+                            $request->request->remove("type");
+                        }
+                    }
                 }
             }
         } else if ($request->isMethod("get")) {
