05c658af86fc7033db159ce69fdbba3b98fb11ca
Chimezie Ugonna <ugiezie@gmail.com>
2022-09-27 07:10:56 +0100

    minor fixes.

--- app/Http/Controllers/PropertyController.php
+++ app/Http/Controllers/PropertyController.php
@@ -55,7 +55,7 @@ class PropertyController extends Controller
         } else {
             return response()->json([
                 "status" => false,
-                "message" => "Property data not found."
+                "message" => "Property data not found." . $request->request->get("property_id")
             ], 404);
         }
     }
