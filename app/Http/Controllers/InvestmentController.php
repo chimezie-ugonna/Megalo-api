276f3de443446fc3de45adf21af572f5145eee7d
Chimezie Ugonna <ugiezie@gmail.com>
2023-01-14 17:13:05 +0100

    minor fixes.

--- app/Http/Controllers/InvestmentController.php
+++ app/Http/Controllers/InvestmentController.php
@@ -196,7 +196,7 @@ class InvestmentController extends Controller
             return response()->json([
                 "status" => true,
                 "message" => "Investment data retrieved successfully.",
-                "data" => Investment::join("users", "users.user_id", "=", "investments;.user_id")->where("property_id", $request->get("property_id"))->get()
+                "data" => Investment::join("users", "users.user_id", "=", "investments.user_id")->where("property_id", $request->get("property_id"))->get()
             ], 200);
         } else {
             return response()->json([
