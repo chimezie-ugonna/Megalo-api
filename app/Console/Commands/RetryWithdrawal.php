a8e202adcf4b84722934cae3b2ae56b1b8f96abc
Chimezie Ugonna <ugiezie@gmail.com>
2023-01-24 15:34:44 +0100

    minor fix.

--- app/Console/Commands/RetryWithdrawal.php
+++ app/Console/Commands/RetryWithdrawal.php
@@ -2,7 +2,6 @@
 
 namespace App\Console\Commands;
 
-use App\Custom\EmailManager;
 use App\Custom\PerformWithdrawal;
 use App\Models\FailedWithdrawal;
 use Illuminate\Console\Command;
@@ -30,10 +29,8 @@ class RetryWithdrawal extends Command
      */
     public function handle()
     {
-        /*if (FailedWithdrawal::count() > 0) {
+        if (FailedWithdrawal::count() > 0) {
             new PerformWithdrawal(FailedWithdrawal::oldest()->first()->user_id, FailedWithdrawal::oldest()->first()->payment_id, FailedWithdrawal::oldest()->first()->amount_usd, "withdrawal", "", "", "", true);
-        }*/
-        $test = new EmailManager();
-        $test->sendOtp("ugiezie@gmail.com", "English", "This a job scheduler test");
+        }
     }
 }
--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,7 +15,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command("retry:withdrawal")->everyMinute()->withoutOverlapping()->runInBackground();
+        $schedule->command("retry:withdrawal")->everyThirtyMinutes()->withoutOverlapping()->runInBackground();
     }
 
     /**
