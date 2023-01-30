74929e6be19be632ada431723f38f5135aef3bb1
Chimezie Ugonna <ugiezie@gmail.com>
2023-01-30 22:49:02 +0100

    minor fixes.

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -36,6 +36,7 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \App\Http\Middleware\EnsureSecureUrl::class
         ],
 
         'api' => [
@@ -63,7 +64,6 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'ensure.secure.url' => \Illuminate\Auth\Middleware\EnsureSecureUrl::class,
         'check.header' => \Illuminate\Auth\Middleware\CheckHeader::class,
         'token.validation' => \Illuminate\Auth\Middleware\TokenValidation::class,
         'incoming.data.validation' => \Illuminate\Auth\Middleware\IncomingDataValidation::class
