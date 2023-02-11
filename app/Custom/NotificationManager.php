c3e7164f6954fc5b568e263a56eaf7a8427f9e77
Chimezie Ugonna <ugiezie@gmail.com>
2023-02-11 03:09:47 +0100

    implementing user identity verification - fix.

--- app/Custom/NotificationManager.php
+++ app/Custom/NotificationManager.php
@@ -73,6 +73,11 @@ class NotificationManager
             ModelsNotification::Create($array);
             unset($array["notification_id"]);
             unset($array["receiver_user_id"]);
+
+            return response()->json([
+              "status" => false,
+              "response" => $response
+            ], 200);
           }
         }
       }
