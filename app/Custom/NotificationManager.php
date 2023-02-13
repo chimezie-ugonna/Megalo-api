0f66ad87b51aef7d25568c3c6627418e6b7c55cf
Chimezie Ugonna <ugiezie@gmail.com>
2023-02-13 17:04:56 +0100

    fixing ability to update device token.

--- app/Custom/NotificationManager.php
+++ app/Custom/NotificationManager.php
@@ -11,83 +11,81 @@ class NotificationManager
 
   function sendNotification($array, $data, $type)
   {
-    /*if ($type == "general") {
-      $device_tokens = Login::all()->pluck("device_token");
+    if ($type == "general") {
+      $user_ids = Login::all()->pluck("user_id")->unique();
     } else {
-      $device_tokens = Login::where("user_id", $array["receiver_user_id"])->where("device_token", "!=", "")->get()->pluck("device_token");
+      $user_ids = [$array["receiver_user_id"]];
     }
-    if (count($device_tokens) > 0) {
-      foreach ($device_tokens as $device_token) {
-        $current_period = date('y-m-d h:i:s');
-        $updated_at = Login::where("device_token", $device_token)->value("device_token_updated_at");
-        $start_datetime = new DateTime($current_period);
-        $end_datetime = new DateTime($updated_at);
-        $diff = $start_datetime->diff($end_datetime);
-        if ($diff->m > 2) {
-          Login::where("device_token", $device_token)->delete();
-        } else {
-          $device_os = Login::where("device_token", $device_token)->value("device_os");
-          if ($device_os == "android" || $device_os == "ios") {
-            $user_id = Login::where("device_token", $device_token)->value("user_id");
-            if (!array_key_exists("notification_id", $array)) {
-              do {
-                $array["notification_id"] = uniqid(rand(), true);
-              } while (Notification::where("notification_id", $array["notification_id"])->exists());
-            }
-            if (!array_key_exists("receiver_user_id", $array)) {
-              $array["receiver_user_id"] = $user_id;
-            }
+    if (count($user_ids) > 0) {
+      foreach ($user_ids as $user_id) {
+        do {
+          $array["notification_id"] = uniqid(rand(), true);
+        } while (Notification::where("notification_id", $array["notification_id"])->exists());
+        $array["receiver_user_id"] = $user_id;
 
-            $ip_address = Login::where("device_token", $device_token)->value("ip_address");
-            if (array_key_exists("title", $array)) {
-              unset($array["title"]);
-            }
-            if (array_key_exists("body", $array)) {
-              unset($array["body"]);
-            }
-            $localization = new Localization($ip_address, $data);
-            $array["title"] = $localization->getText($array["title_key"]);
-            $array["body"] = $localization->getText($array["body_key"]);
+        $ip_address = Login::where("user_id", $user_id)->latest("updated_at")->first()->ip_address;
+        $localization = new Localization($ip_address, $data);
+        $array["title"] = $localization->getText($array["title_key"]);
+        $array["body"] = $localization->getText($array["body_key"]);
 
-            if ($device_os == "android") {
-              $priority = "high";
+        $device_tokens = Login::where("user_id", $user_id)->where("device_token", "!=", "")->get()->pluck("device_token");
+        if (count($device_tokens) > 0) {
+          foreach ($device_tokens as $device_token) {
+            $current_period = date('y-m-d h:i:s');
+            $updated_at = Login::where("device_token", $device_token)->value("device_token_updated_at");
+            $start_datetime = new DateTime($current_period);
+            $end_datetime = new DateTime($updated_at);
+            $diff = $start_datetime->diff($end_datetime);
+            if ($diff->m > 2) {
+              Login::where("device_token", $device_token)->delete();
             } else {
-              $priority = "10";
-            }
+              $device_os = Login::where("device_token", $device_token)->value("device_os");
+              if ($device_os == "android" || $device_os == "ios") {
+                $ip_address = Login::where("device_token", $device_token)->value("ip_address");
+                $localization = new Localization($ip_address, $data);
+                $title = $localization->getText($array["title_key"]);
+                $body = $localization->getText($array["body_key"]);
 
-            $notification = ["title" => $array["title"], "body" => $array["body"], "sound" => "notifications.mp3", "icon" => "notification_icon", "android_channel_id" => "megalo_general_channel_id"];
-            $json = json_encode(["to" => $device_token, "notification" => $notification, "data" => array_merge($data, $notification), "priority" => $priority]);
-            $curl = curl_init();
+                if ($device_os == "android") {
+                  $priority = "high";
+                } else {
+                  $priority = "10";
+                }
 
-            curl_setopt_array($curl, array(
-              CURLOPT_URL => "https://fcm.googleapis.com/fcm/send",
-              CURLOPT_HTTPHEADER => array(
-                "Content-Type: application/json",
-                "Authorization: key=" . getenv("FCM_SERVER_KEY")
-              ),
-              CURLOPT_RETURNTRANSFER => true,
-              CURLOPT_POST => true,
-              CURLOPT_POSTFIELDS => $json,
-              CURLOPT_CUSTOMREQUEST => "POST"
-            ));
+                $notification = ["title" => $title, "body" => $body, "sound" => "notifications.mp3", "icon" => "notification_icon", "android_channel_id" => "megalo_general_channel_id"];
+                $json = json_encode(["to" => $device_token, "notification" => $notification, "data" => array_merge($data, $notification), "priority" => $priority]);
+                $curl = curl_init();
 
-            $response = curl_exec($curl);
+                curl_setopt_array($curl, array(
+                  CURLOPT_URL => "https://fcm.googleapis.com/fcm/send",
+                  CURLOPT_HTTPHEADER => array(
+                    "Content-Type: application/json",
+                    "Authorization: key=" . getenv("FCM_SERVER_KEY")
+                  ),
+                  CURLOPT_RETURNTRANSFER => true,
+                  CURLOPT_POST => true,
+                  CURLOPT_POSTFIELDS => $json,
+                  CURLOPT_CUSTOMREQUEST => "POST"
+                ));
 
-            $responseData = json_decode($response, true);
-            if (isset($responseData["results"][0]["error"])) {
-              $error_message = $responseData["results"][0]["error"];
-              if ($error_message == "NotRegistered" || $error_message == "InvalidRegistration") {
-                Login::where("device_token", $device_token)->delete();
+                $response = curl_exec($curl);
+
+                $responseData = json_decode($response, true);
+                if (isset($responseData["results"][0]["error"])) {
+                  $error_message = $responseData["results"][0]["error"];
+                  if ($error_message == "NotRegistered" || $error_message == "InvalidRegistration") {
+                    Login::where("device_token", $device_token)->delete();
+                  }
+                }
+
+                curl_close($curl);
               }
             }
-
-            curl_close($curl);
           }
         }
+
         Notification::Create($array);
-        unset($array["notification_id"]);
-        unset($array["receiver_user_id"]);
       }
-    }*/
+    }
   }
 }
