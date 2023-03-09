4aac11c8c9feb0fd476755c5ad81df97314b3a64
Chimezie Ugonna <ugiezie@gmail.com>
2023-03-09 15:36:47 +0100

    implementing redirect to https.

--- app/Http/Middleware/EnsureSecureUrl.php
+++ app/Http/Middleware/EnsureSecureUrl.php
@@ -16,9 +16,9 @@ class EnsureSecureUrl
      */
     public function handle(Request $request, Closure $next)
     {
-        /*if (!$request->secure()) {
-            return redirect()->secure($request->getRequestUri());
-        }*/
+        if (!$request->secure()) {
+            return redirect()->secure($request->path());
+        }
         return $next($request);
     }
 }
