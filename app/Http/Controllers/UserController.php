28462d812402e41cbe5e3bc281107bd836da744c
Chimezie Ugonna <ugiezie@gmail.com>
2023-05-18 13:05:16 +0100

    minor fix.

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -255,7 +255,7 @@ class UserController extends Controller
         return response()->json([
             "status" => true,
             "message" => "All user data retrieved successfully.",
-            "data" => User::latest()->paginate(10)
+            "data" => User::latest()->simplePaginate(10)
         ], 200);
     }
 
