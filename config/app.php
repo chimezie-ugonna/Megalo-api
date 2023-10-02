bf924395b7074c2d8adab104e894d6e1e80d2de4
Chimezie Ugonna <ugiezie@gmail.com>
2023-10-02 04:20:04 +0100

    minor fixes.

--- config/app.php
+++ config/app.php
@@ -194,7 +194,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        App\Providers\Jenssegers\Agent\AgentServiceProvider::class,
+        Jenssegers\Agent\AgentServiceProvider::class,
 
     ],
 
